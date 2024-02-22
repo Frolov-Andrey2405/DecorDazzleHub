86cda80d5676ce53b2b34e8855ccd40e24bcd8fd
Andrey Frolov <senior.slender2015@gmail.com>
2024-02-22 14:43:17 +0200

    MTV Design Template (MVC)

--- app/main/views.py
+++ app/main/views.py
@@ -1,5 +1,14 @@
 """Views"""
 
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    """View function for home page of site"""
+    return HttpResponse("Home page")
+
+
+def about(request):
+    """View function for about page of site"""
+    return HttpResponse("About page")
