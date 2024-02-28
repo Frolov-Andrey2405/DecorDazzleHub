438f60b4301cb892d0e6c2543c6c7d8471277f8f
Andrey Frolov <senior.slender2015@gmail.com>
2024-02-28 16:36:06 +0200

    Routing url addresses - include, namespace

--- app/app/urls.py
+++ app/app/urls.py
@@ -1,11 +1,10 @@
 """URL configuration for app project"""
 
-from main import views
+
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
-    path("", views.index, name="index"),
     path("admin/", admin.site.urls),
-    path("about/", views.about, name="about"),
+    path("", include("main.urls", namespace="main")),
 ]
---
+++ app/main/urls.py
@@ -0,0 +1,11 @@
+"""URL configuration for main project"""
+
+from django.urls import path
+from main import views
+
+app_name = "main"
+
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("about/", views.about, name="about"),
+]
