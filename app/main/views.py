841b313a1512e0e62211904efb5a075aeb83218d
Andrey Frolov <senior.slender2015@gmail.com>
2024-02-28 16:16:41 +0200

    Template inheritance, extends tag, base template

--- app/main/views.py
+++ app/main/views.py
@@ -1,17 +1,13 @@
 """Views"""
 
-from django.http import HttpResponse
 from django.shortcuts import render
 
 
 def index(request):
     """View function for home page of site"""
     context = {
-        "title": "DecorDazzleHub",
-        "content": "Page content",
-        "list": ["first", "second"],
-        "dict": {"first": 1},
-        "is_authenticated": True,
+        "title": "DecorDazzleHub - Main",
+        "content": "Furniture store 'DecorDazzleHub'",
     }
 
     return render(request, 'main/index.html', context)
@@ -19,4 +15,10 @@ def index(request):
 
 def about(request):
     """View function for about page of site"""
-    return HttpResponse("About page")
+    context = {
+        "title": "DecorDazzleHub - About us",
+        "content": "About us",
+        "text_on_page": "Lorem"
+    }
+
+    return render(request, 'main/about.html', context)
