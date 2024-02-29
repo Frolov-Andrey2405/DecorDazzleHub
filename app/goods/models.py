6dedce4b6afe0565a100ca6e6e317edb7de5691a
Andrey Frolov <senior.slender2015@gmail.com>
2024-02-29 16:22:56 +0200

    Migrations, model building

--- app/goods/models.py
+++ app/goods/models.py
@@ -2,4 +2,10 @@
 
 from django.db import models
 
-# Create your models here.
+
+class Categories(models.Model):
+    class Meta:
+        db_table = "categories"
+
+    name = models.CharField(max_length=150, unique=True)
+    slug = models.SlugField(max_length=200, unique=True, blank=True, null=True)
