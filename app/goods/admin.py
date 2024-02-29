ed1bd37e63522c86fcffaa0fb34cfbad6b03797d
Andrey Frolov <senior.slender2015@gmail.com>
2024-02-29 16:39:09 +0200

    Simple model registration, initial display setup

--- app/goods/admin.py
+++ app/goods/admin.py
@@ -1,5 +1,6 @@
 """Admin site for goods app"""
 
 from django.contrib import admin
+from goods.models import Categories
 
-# Register your models here.
+admin.site.register(Categories)
--- app/goods/models.py
+++ app/goods/models.py
@@ -6,6 +6,10 @@ from django.db import models
 class Categories(models.Model):
     class Meta:
         db_table = "categories"
+        verbose_name = "Category"
+        verbose_name_plural = "Categories"
 
-    name = models.CharField(max_length=150, unique=True)
-    slug = models.SlugField(max_length=200, unique=True, blank=True, null=True)
+    name = models.CharField(max_length=150, unique=True, verbose_name="Name")
+    slug = models.SlugField(
+        max_length=200, unique=True, blank=True, null=True, verbose_name="URL"
+    )
