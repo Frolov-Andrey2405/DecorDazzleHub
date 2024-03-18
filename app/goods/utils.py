470d816c773edd021426eeda6bd95632215b1aed
Andrey Frolov <senior.slender2015@gmail.com>
2024-03-18 20:21:52 +0200

    Q Objects

--- app/goods/utils.py
+++ app/goods/utils.py
@@ -1,13 +1,24 @@
 """The utils module contains helper functions for the goods app"""
 
+from django.db.models import Q
 from goods.models import Product
 
 
 def q_search(query):
     """
-    The q_search function is a helper function that allows the user to search for products by their id.
-    If the query is an integer and has less than or equal to 5 digits, then it will return all products with that id.
-
+    The q_search function takes a query string and returns a queryset of products.
     """
     if query.isdigit() and len(query) <= 5:
         return Product.objects.filter(id=int(query))
+
+    keywords = [
+        word for word in query.split() if len(word) > 2
+    ]
+
+    q_object = Q()
+
+    for token in keywords:
+        q_object |= Q(description__icontains=token)
+        q_object |= Q(name__icontains=token)
+
+    return Product.objects.filter(q_object)
