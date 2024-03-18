c7f181745c54d40756731b9be5fcd070e79e1d09
Andrey Frolov <senior.slender2015@gmail.com>
2024-03-18 14:06:08 +0200

    Working with ORM in controllers

--- app/goods/views.py
+++ app/goods/views.py
@@ -1,26 +1,18 @@
 """Views"""
 
-import json
-import os
-
 from django.shortcuts import render
 
+from goods.models import Product
 
 def catalog(request):
     """
     The catalog function is the main page of the website.
     """
-
-    # Constructing the file path safely
-    file_path = os.path.join(os.getcwd(), "app", "goods", "goods_list.json")
-
-    # Reading data from goods_list.json
-    with open(file_path, "r", encoding="utf-8") as json_file:
-        goods_data = json.load(json_file)
+    goods = Product.objects.all()
 
     context = {
         "title": "DecorDazzleHub - Catalog",
-        "goods": goods_data,
+        "goods": goods,
     }
 
     return render(request, "goods/catalog.html", context)
