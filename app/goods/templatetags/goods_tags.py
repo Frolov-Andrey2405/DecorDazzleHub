09b388c05817b8db2b2b11cbc824dd44cfa0d95e
Andrey Frolov <senior.slender2015@gmail.com>
2024-03-18 19:15:23 +0200

    Custom tag with parameters, urlencode, passing arguments

--- app/goods/templatetags/goods_tags.py
+++ app/goods/templatetags/goods_tags.py
@@ -1,6 +1,7 @@
 """Tags for goods app"""
 
 from django import template
+from django.utils.http import urlencode
 
 from goods.models import Categories
 
@@ -13,3 +14,13 @@ def tag_categories():
     Returns all the categories in the database.
     """
     return Categories.objects.all()
+
+@register.simple_tag(takes_context=True)
+def change_params(context, **kwargs):
+    """
+    The change_params function takes a context and any number of keyword arguments.
+    It returns a query string that is the result of updating the current request's GET parameters with those passed in as kwargs.
+    """
+    query = context["request"].GET.dict()
+    query.update(kwargs)
+    return urlencode(query)
