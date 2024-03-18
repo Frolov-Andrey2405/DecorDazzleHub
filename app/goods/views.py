d50b4ab4ab815aa5378321800019f3ac5a79c837
Andrey Frolov <senior.slender2015@gmail.com>
2024-03-18 17:33:42 +0200

    Reverse resolution of url addresses

--- app/goods/views.py
+++ app/goods/views.py
@@ -1,14 +1,18 @@
 """Views"""
 
-from django.shortcuts import render
+from django.shortcuts import get_list_or_404, render
 
 from goods.models import Product
 
-def catalog(request):
+def catalog(request, category_slug):
     """
     The catalog function is the main page of the website.
     """
-    goods = Product.objects.all()
+
+    if category_slug == "all":
+        goods = Product.objects.all()
+    else:
+        goods = get_list_or_404(Product.objects.filter(category__slug=category_slug))
 
     context = {
         "title": "DecorDazzleHub - Catalog",
@@ -18,11 +22,11 @@ def catalog(request):
     return render(request, "goods/catalog.html", context)
 
 
-def product(request, product_id):
+def product(request, product_slug):
     """
     The product function returns a rendered template of the product page.
     """
-    product_instance = Product.objects.get(id=product_id)
+    product_instance = Product.objects.get(slug=product_slug)
 
     context = {
         "product": product_instance,
