9d1c6bcb7b94711e26dd4b941ccbe3bc55d0e654
Andrey Frolov <senior.slender2015@gmail.com>
2024-03-20 15:16:28 +0200

    Standard form handling, POST requests, model-related forms, user authorization, real form handling in Django

--- app/users/views.py
+++ app/users/views.py
@@ -1,11 +1,34 @@
 """Views"""
 
+from django.contrib import auth
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
+
+from users.forms import UserLoginForm
 
 def login(request):
-    """View function for login page of site"""
+    """
+    The login function is a view that allows users to login.
+    It takes in the request and returns a rendered template of the login page.
+    If the user submits valid credentials,
+    they are authenticated and redirected to their profile page.
+    """
+    if request.method == "POST":
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST["username"]
+            password = request.POST["password"]
+            user = auth.authenticate(username=username, password=password)
+            if user:
+                auth.login(request, user)
+                return HttpResponseRedirect(reverse("main:index"))
+    else:
+        form = UserLoginForm()
+
     context = {
         "title": "DecorDazzleHub - Login",
+        "form": form,
     }
 
     return render(request, "users/login.html", context)
