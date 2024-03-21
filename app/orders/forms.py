263a547c0f809d3c35b6d1688ec19cbcc632ba38
Andrey Frolov <senior.slender2015@gmail.com>
2024-03-21 20:44:41 +0200

    Custom validators

--- app/orders/forms.py
+++ app/orders/forms.py
@@ -1,5 +1,6 @@
 """Forms"""
 
+import re
 from django import forms
 
 
@@ -8,12 +9,15 @@ class CreateOrderForm(forms.Form):
     Form for creating a new order.
 
     Fields:
-        first_name (CharField): First name of the client
-        last_name (CharField): Last name of the client
-        phone_number (CharField): Phone number of the client
-        requires_delivery (ChoiceField): Does the order require delivery (boolean)
-        delivery_address (CharField): Address for delivery (optional)
-        payment_on_get (ChoiceField): Is the order paid on delivery (boolean)
+        first_name (CharField): User's first name
+        last_name (CharField): User's last name
+        phone_number (CharField): User's phone number
+        requires_delivery (ChoiceField): Delivery required (True/False)
+        delivery_address (CharField): Delivery address (optional)
+        payment_on_get (ChoiceField): Payment on receipt (True/False)
+
+    Methods:
+        clean_phone_number(self): Validates phone number format
     """
 
     first_name = forms.CharField()
@@ -32,3 +36,18 @@ class CreateOrderForm(forms.Form):
             ("1", 'True'),
             ],
         )
+
+    def clean_phone_number(self):
+        """
+        Validates the format of the phone number.
+        """
+        data = self.cleaned_data['phone_number']
+
+        if not data.isdigit():
+            raise forms.ValidationError("Номер телефона должен содержать только цифры")
+
+        pattern = re.compile(r'^\d{10}$')
+        if not pattern.match(data):
+            raise forms.ValidationError("Неверный формат номера")
+
+        return data
